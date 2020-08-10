9cebbdb0afdc40b46b51ea10a780a32f957243e1
al_chaudhari <chaudharia041@gmail.com>
2020-08-11 02:18:24 +0530

    Added Comments to Doit Folder

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 const path = require("path"),
-  home = require("os").homedir(),
-  doit_path = path.join(home, ".doit"),
-  doit_profile = path.join(doit_path, "profile.json"),
-  doit_projects = path.join(doit_path, "project.json"),
-  doit_tags = path.join(doit_path, "tags.json");
+  home = require("os").homedir(), // Home Directory
+  doit_path = path.join(home, ".doit"), // Doit Foler for Configurations
+  doit_profile = path.join(doit_path, "profile.json"), // Doit Profile File
+  doit_projects = path.join(doit_path, "project.json"), // Doit Projects File
+  doit_tags = path.join(doit_path, "tags.json"); // Doit Tags File
 
 module.exports = {
   home: home,
